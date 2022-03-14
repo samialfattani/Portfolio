0096cda21e87de4ee7ba5d694a3c53c815f67dd6
Sami Alfattani <Sami_alfattani@hotmail.com>
2022-03-14 20:28:24 +0300

    third commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Portfolio
 My Portfolio
+
+
+## Template Name: iPortfolio
+
+Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+
+Author: BootstrapMade.com
+
+License: https://bootstrapmade.com/license/
--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,7 +6,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('.php-email-formxxxxxxx');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
@@ -83,3 +83,4 @@
   }
 
 })();
+
